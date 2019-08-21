7496044b4df33f02d36171fd8e1a00c452b86ade
congzw <2981810300@qq.com>
2019-08-21 11:13:25 +0800

    init projects

---
+++ readme.md
@@ -0,0 +1,8 @@
+# demo for how to run app as admin by app.manifest settings in build time
+
+# how to?
+
+## change list
+
+- 
+- 20190821 init project
---
+++ src/FooApp/RoleHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Principal;
+
+namespace FooApp
+{
+    public class RoleHelper
+    {
+        public static bool IsRunAsAdmin()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
